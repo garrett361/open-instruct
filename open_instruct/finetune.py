4e06f786084e5c3765a948e35021170092654bed
Garrett Goon <garrett361@gmail.com>
2025-01-17 21:37:59 -0500

    len(train_dataset) -> len(train_dataloader)

--- open_instruct/finetune.py
+++ open_instruct/finetune.py
@@ -915,7 +915,7 @@ def main(args: FlatArguments):
     total_batch_size = args.per_device_train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
 
     logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(train_dataset)}")
+    logger.info(f"  Num examples = {len(train_dataloader)}")
     logger.info(f"  Num Epochs = {args.num_train_epochs}")
     logger.info(f"  Instantaneous batch size per device = {args.per_device_train_batch_size}")
     logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
