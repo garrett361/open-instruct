7365706b9270b03de6c44882b729da3151803cd9
Garrett Goon <garrett361@gmail.com>
2025-01-17 20:33:30 -0500

    print args

--- open_instruct/finetune.py
+++ open_instruct/finetune.py
@@ -832,6 +832,7 @@ def main(args: FlatArguments):
     if accelerator.process_index == 0:
         print(f"{model=}")
         print(f"{accelerator.state.fsdp_plugin=}")
+        print(f"{args=}")
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
