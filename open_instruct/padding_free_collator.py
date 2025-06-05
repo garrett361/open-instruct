4ab607ea394764a8761aee4a542c136789ea5b95
Garrett Goon <goon@ibm.com>
2025-06-05 15:23:32 -0400

    add seq_idx to collator

--- open_instruct/padding_free_collator.py
+++ open_instruct/padding_free_collator.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 
 import torch
 from transformers import DefaultDataCollator
-from transformers.data.data_collator import default_data_collator
 
 
 @dataclass
@@ -16,9 +15,19 @@ class TensorDataCollatorWithFlattening(DefaultDataCollator):
     - no padding will be added, returns `input_ids`, `labels` and `position_ids`
     """
 
-    def __init__(self, *args, return_position_ids=True, separator_id=-100, **kwargs):
+    def __init__(
+        self,
+        *args,
+        return_flash_attn_kwargs=True,
+        return_position_ids=True,
+        return_seq_idx=True,
+        separator_id=-100,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
+        self.return_flash_attn_kwargs = return_flash_attn_kwargs
         self.return_position_ids = return_position_ids
+        self.return_seq_idx = return_seq_idx
         self.separator_id = separator_id
         warnings.warn(
             "Using `DataCollatorWithFlattening` will flatten the entire mini batch into single long sequence."
@@ -30,16 +39,24 @@ class TensorDataCollatorWithFlattening(DefaultDataCollator):
             return_tensors = self.return_tensors
         if separator_id is None:
             separator_id = self.separator_id
+        assert self.return_flash_attn_kwargs, (
+            "Only should be used with return_flash_attn_kwargs=True"
+        )
+        if self.return_flash_attn_kwargs:
+            cu_seq_lens = [0]
+            max_length = 0
+        if self.return_position_ids:
+            pos_ids = []
+        if self.return_seq_idx:
+            seq_idx = []
         is_labels_provided = "labels" in features[0]
         ret = {"input_ids": [], "labels": []}
-        if self.return_position_ids:
-            ret.update({"position_ids": []})
         separator = torch.tensor(
             [separator_id],
             dtype=features[0]["input_ids"].dtype,
             device=features[0]["input_ids"].device,
         )
-        for item in features:
+        for seq_idx, item in enumerate(features):
             input_ids = item["input_ids"]
             ret["input_ids"].append(input_ids)
             if is_labels_provided:
@@ -48,13 +65,23 @@ class TensorDataCollatorWithFlattening(DefaultDataCollator):
             else:
                 ret["labels"].append(separator)
                 ret["labels"].append(input_ids[1:])
+            if self.return_flash_attn_kwargs:
+                cu_seq_lens.append(cu_seq_lens[-1] + len(input_ids))
+                max_length = max(max_length, len(input_ids))
             if self.return_position_ids:
-                ret["position_ids"].append(
-                    torch.arange(
-                        input_ids.numel(),
-                        dtype=input_ids.dtype,
-                        device=input_ids.device,
-                    )
-                )
-        ret = {k: torch.cat(v, dim=0) for k, v in ret.items()}
-        return default_data_collator([ret], return_tensors)
+                pos_ids.append(torch.arange(input_ids.numel(), device=input_ids.device))
+            if self.return_seq_idx:
+                seq_idx.append(torch.full_like(input_ids, seq_idx, dtype=torch.float32))
+
+        if self.return_flash_attn_kwargs:
+            ret["cu_seq_lens_q"] = ret["cu_seq_lens_k"] = torch.tensor(
+                cu_seq_lens, dtype=torch.int32, device=features[0]["input_ids"].device
+            )
+            ret["max_length_q"] = ret["max_length_k"] = max_length
+        if self.return_position_ids:
+            ret["position_ids"] = torch.cat(pos_ids, dim=0)[None]
+        if self.return_seq_idx:
+            ret["seq_idx"] = torch.cat(seq_idx, dim=0)[None]
+        ret["input_ids"] = torch.cat(ret["input_ids"], dim=0)[None]
+        ret["labels"] = torch.cat(ret["labels"], dim=0)[None]
+        return ret
