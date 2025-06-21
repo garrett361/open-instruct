2dd8bbadbe83489e52310abf8e832b6bf99f3cf3
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-06-21 20:46:57 +0000

    improve

--- scripts/test_dataloading.py
+++ scripts/test_dataloading.py
@@ -1,60 +1,36 @@
-from open_instruct.finetune import main
-import importlib
+import torch
+import os
 
-from typing import Any
 from unittest.mock import patch, Mock
 from accelerate import Accelerator
 
-def patch_target_module(
-    to_patch: str,
-    replace_with: Any,
-    target_module: str = None,
-):
-    to_patch = to_patch.split(".")
-    assert len(to_patch) > 1, "must have an object to patch"
 
-    to_patch, obj_name_to_patch = to_patch[:-1], to_patch[-1]
-    to_patch = ".".join(to_patch)
-    source = importlib.import_module(to_patch)
-    original_obj = getattr(source, obj_name_to_patch)
-    setattr(source, obj_name_to_patch, replace_with)
-
-    if target_module is not None:
-        # reload and this should get the patched object
-        target_module = importlib.import_module(target_module)
-        importlib.reload(target_module)
-
-        # replace it
-        setattr(source, obj_name_to_patch, original_obj)
-
-class DummyAccelerator:
-
-    def __init__(*args, **kwargs):
-        pass
-
-
-from transformers import AutoModelForCausalLM, AutoConfig
+from transformers import AutoModelForCausalLM
 from accelerate import init_empty_weights
-from accelerate.state import AcceleratorState
-from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
-# MODEL_CONFIG = None
-# def from_pretrained(model_name_or_path, *args, **kwargs):
-#     global MODEL_CONFIG
-#     MODEL_CONFIG = AutoConfig.from_pretrained(model_name_or_path)
-#     return Mock()
 from types import MethodType
 
 STORE = []
 
+# builds the from_pretrained to pass through AutoModelForCausalLM
+# - loads the model on the meta device
+
 def built_from_pretrained():
 
     def forward(self, input_ids, *args, **kwargs):
-        STORE.append(input_ids)
+        # - hook to capture the input_ids
+        STORE.append({
+            'input_ids': input_ids,
+            'labels': kwargs.get('labels'),
+        })
+
+        # - returns dummy outputs / loss
         return CausalLMOutputWithPast(
             loss=torch.tensor(0.),
         )
 
+    # mock the from_pretrained function
     old_func = AutoModelForCausalLM.from_pretrained
     def _from_pretrained(model_name_or_path, *args, **kwargs):
         with init_empty_weights():
@@ -66,14 +42,11 @@ def built_from_pretrained():
 
     return _from_pretrained
 
-patch_transformers = patch.multiple(
-    AutoModelForCausalLM,
-    # from_pretrained=Mock(),
-    from_pretrained=built_from_pretrained(),
-)
-
 def accelerate_prepare(self, model, optimizer, dataloader, scheduler):
     self.state = Mock() # sneak it in
+
+    # skip through the set_epoch if the train loop assumes a distributed
+    # dataloader
 
     def set_epoch(self, *args):
         pass
@@ -81,7 +54,12 @@ def accelerate_prepare(self, model, optimizer, dataloader, scheduler):
     dataloader.set_epoch = MethodType(set_epoch, dataloader)
     return model, optimizer, dataloader, scheduler
 
-import torch
+# - patches
+
+patch_transformers = patch.multiple(
+    AutoModelForCausalLM,
+    from_pretrained=built_from_pretrained(),
+)
 
 patch_accelerate = patch.multiple(
     Accelerator,
@@ -93,21 +71,44 @@ patch_accelerate = patch.multiple(
     sync_gradients=False,
 )
 
-# patch_accelerate_state = patch.multiple(
-#     AcceleratorState,
-#     fsdp_plugin=Mock(),
-# )
+PATCHES = [
+    patch_transformers,
+    patch_accelerate,
+]
 
-# import deepspeed
-# import torch
-# 
-# def deepspeed_enter(*args, **kargs):
-#     MODEL_CONFIG.
-#     return torch.nn.Embedding(
-# 
-#     )
-# 
-# patch_deepspeed = patch.multiple(
-#     deepspeed.zero.GatheredParameters,
-#     __enter__=
-# )
+def test_finetune(
+    model_name_or_path: str,
+    train_file: str,
+    max_train_steps: int = 2,
+    write_data_to_directory: str = None,
+):
+    from open_instruct.finetune import main, FlatArguments
+    from contextlib import ExitStack
+
+    args = FlatArguments(
+        model_name_or_path=model_name_or_path,
+        train_file=train_file,
+        push_to_hub=False,
+        try_launch_beaker_eval_jobs=False,
+        output_dir=None,
+        max_train_steps=max_train_steps,
+    )
+
+    # clear the store
+    STORE.clear()
+    with ExitStack() as stack:
+        for patch in PATCHES:
+            stack.enter_context(patch)
+        main(args)
+
+    if write_data_to_directory is None:
+        return STORE
+
+    os.makedirs(write_data_to_directory)
+    for i, data in enumerate(STORE):
+        torch.save(data, os.path.join(write_data_to_directory, f'batch_{i}.pt'))
+
+if __name__ == "__main__":
+    import fire
+    data = fire.Fire(test_finetune)
+    
